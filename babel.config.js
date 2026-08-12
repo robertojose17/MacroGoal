4a2a5605d0fa2464ba95f9ceedb0dd8fdf141a29
newly.dev <api@newly.dev>
2026-08-12 21:28:08 +0000

    fix: Remove missing babel plugin references from babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const EDITABLE_COMPONENTS =
-    process.env.EXPO_PUBLIC_ENABLE_EDIT_MODE === "TRUE" &&
-    process.env.NODE_ENV === "development"
-      ? [
-          ["./babel-plugins/editable-elements.js", {}],
-          ["./babel-plugins/inject-source-location.js", {}],
-        ]
-      : [];
-
   return {
     presets: ["babel-preset-expo"],
     plugins: [
@@ -38,7 +29,6 @@ module.exports = function (api) {
           },
         },
       ],
-      ...EDITABLE_COMPONENTS,
       "@babel/plugin-proposal-export-namespace-from",
       // NOTE: react-native-reanimated/plugin is intentionally REMOVED.
       // The real react-native-reanimated native module is NOT linked in the
