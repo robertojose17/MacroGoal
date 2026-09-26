cc464c27bf81ebea8628c8f61111ffd1d02d7043
newly.dev <api@newly.dev>
2026-09-26 00:06:03 +0000

    fix: Remove react-native-worklets and alias in Metro

--- metro.config.js
+++ metro.config.js
@@ -7,6 +7,11 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.unstable_enablePackageExports = true;
 
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  'react-native-worklets': require.resolve('./stubs/react-native-worklets.js'),
+};
+
 // Use turborepo to restore the cache when possible
 config.cacheStores = [
     new FileStore({ root: path.join(__dirname, 'node_modules', '.cache', 'metro') }),
